bed31e5ad0f7f4c642c139569ea468996f55aabb
moshebe123@gmail.com <moshebe123@gmail.com@86056eeb-25c0-f91b-d89b-f80b2ee9bc02>
2013-10-02 15:55:00 +0000

    Change the blood pressure to a custom view. currently supports only English.
    git-svn-id: https://mada-reports.googlecode.com/svn/trunk@144 86056eeb-25c0-f91b-d89b-f80b2ee9bc02

--- MadaReports/src/com/mdareports/ui/activities/details/TechInfoFragment.java
+++ MadaReports/src/com/mdareports/ui/activities/details/TechInfoFragment.java
@@ -1,36 +1,22 @@
 package com.mdareports.ui.activities.details;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.TextView;
 
 import com.mdareports.R;
 import com.mdareports.db.models.Report;
+import com.mdareports.ui.custom.bloodpressure.BloodPressureField;
 import com.mdareports.ui.custom.single.EditableSeekField;
-import com.mdareports.utils.ApplicationUtils;
-import com.mdareports.utils.DeviceInfoUtils;
-import com.mdareports.utils.rangeseekbar.RangeSeekBar;
-import com.mdareports.utils.rangeseekbar.RangeSeekBar.OnRangeSeekBarChangeListener;
 
 public class TechInfoFragment extends FragmentDetailActivity {
 
 	private EditableSeekField esFieldPulse;
 	private EditableSeekField esFieldSugar;
 	private EditableSeekField esFieldBreath;
-
-	private RangeSeekBar<Integer> bloodPressureView;
-
-	private TextView bloodPressureValue;
+	private BloodPressureField bloodPressureField;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,149 +39,12 @@ public class TechInfoFragment extends FragmentDetailActivity {
 		esFieldBreath = (EditableSeekField) activity
 				.findViewById(R.id.esfieldBreath);
 
-		// TODO: move all this blood pressure shit from here.
-
-		// set the blood pressure views
-		bloodPressureView = new RangeSeekBar<Integer>(getResources()
-				.getInteger(R.integer.min_blood_pressure), getResources()
-				.getInteger(R.integer.max_blood_pressure), activity);
-
-		// Init the layout of the ploodPressureView
-		RelativeLayout thisLayout = (RelativeLayout) activity
-				.findViewById(R.id.bloodPressureLayout);
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-				ApplicationUtils.getDpiFromInteger(
-						getResources().getInteger(
-								R.integer.blood_pressure_view_width_dimension),
-						activity),
-				ApplicationUtils
-						.getDpiFromInteger(
-								getResources()
-										.getInteger(
-												R.integer.blood_pressure_view_height_dimension),
-								activity));
-		params.setMargins(0, 25, 0, 20);
-		
-		if (DeviceInfoUtils.isCurrentLanguageHebrew(activity)) {
-			params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-		} else {
-			params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-		}
-		thisLayout.addView(bloodPressureView, params);
-
-		// initialize the bloodPressureValue view by the bloodPressureView
-		bloodPressureValue = (TextView) activity
-				.findViewById(R.id.bloodPressureValue);
-		bloodPressureValue.setText(bloodPressureView.getSelectedMinValue()
-				+ ", " + bloodPressureView.getSelectedMaxValue());
-		bloodPressureView
-				.setOnRangeSeekBarChangeListener(new OnRangeSeekBarChangeListener<Integer>() {
-					@Override
-					public void onRangeSeekBarValuesChanged(
-							RangeSeekBar<?> bar, Integer minValue,
-							Integer maxValue) {
-						bloodPressureValue.setText(String.format("%d - %d",
-								minValue, maxValue));
-					}
-				});
-		bloodPressureValue.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				openDialogForBloodPressure(
-						getString(R.string.fragment_tech_info_blood_pressure),
-						bloodPressureView, bloodPressureValue);
-			}
-		});
+		bloodPressureField = (BloodPressureField) activity
+				.findViewById(R.id.bloodPressureField);
 
 		// load the details from the report into the fields
 		refreshDataWithCurrentReport();
-		
-		bloodPressureView.setEnabled(false);
-	}
 
-	/**
-	 * Add a {@link OnSeekBarChangeListener} to the {@link TextView}</br> and
-	 * when the {@link SeekBar} is changed, also change the text in the
-	 * {@link TextView}
-	 * 
-	 * @param seekBar
-	 *            {@link SeekBar} to listen to
-	 * @param changingTextView
-	 *            The {@link TextView} that suppose to listen to the
-	 *            {@link SeekBar}
-	 */
-
-	private void openDialogForBloodPressure(String title,
-			final RangeSeekBar<Integer> bloodPressureViewFromFragemnt,
-			final TextView bloodPressureTextValueFromFragment) {
-		LayoutInflater inflater = (LayoutInflater) getActivity()
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		final View formView = inflater.inflate(
-				R.layout.dialog_seekbar_blood_pressure, null, false);
-
-		// set the blood pressure views
-		final RangeSeekBar<Integer> newBloodPressureView = new RangeSeekBar<Integer>(
-				getResources().getInteger(R.integer.min_blood_pressure),
-				getResources().getInteger(R.integer.max_blood_pressure),
-				getActivity());
-		
-		newBloodPressureView.setSelectedMinValue(bloodPressureViewFromFragemnt
-				.getSelectedMinValue());
-		newBloodPressureView.setSelectedMaxValue(bloodPressureViewFromFragemnt
-				.getSelectedMaxValue());
-
-		// Init the layout of the ploodPressureView
-		RelativeLayout thisLayout = (RelativeLayout) formView
-				.findViewById(R.id.bloodPressureDialogLayout);
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.MATCH_PARENT, getResources()
-						.getInteger(
-								R.integer.blood_pressure_view_height_dimension));
-		params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-		thisLayout.addView(newBloodPressureView, params);
-
-		// Init the bloodPressureValue view by the bloodPressureView
-		final TextView newBloodPressureText = (TextView) formView
-				.findViewById(R.id.dialogBloodPressureTextView);
-		newBloodPressureText.setText(bloodPressureTextValueFromFragment
-				.getText());
-		newBloodPressureView
-				.setOnRangeSeekBarChangeListener(new OnRangeSeekBarChangeListener<Integer>() {
-					@Override
-					public void onRangeSeekBarValuesChanged(
-							RangeSeekBar<?> bar, Integer minValue,
-							Integer maxValue) {
-						newBloodPressureText.setText(String.format("%d - %d",
-								minValue, maxValue));
-					}
-				});
-
-		new AlertDialog.Builder(getActivity())
-				.setView(formView)
-				.setTitle(title)
-				.setPositiveButton(
-						R.string.fragment_tech_info_dialog_positive_button,
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int id) {
-								bloodPressureViewFromFragemnt
-										.setSelectedMinValue(newBloodPressureView
-												.getSelectedMinValue());
-								bloodPressureViewFromFragemnt
-										.setSelectedMaxValue(newBloodPressureView
-												.getSelectedMaxValue());
-								bloodPressureTextValueFromFragment
-										.setText(newBloodPressureText.getText());
-							}
-						})
-				.setNegativeButton(
-						R.string.fragment_tech_info_dialog_negative_button,
-						new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								dialog.cancel();
-							}
-						}).show();
 	}
 
 	@Override
@@ -207,10 +56,9 @@ public class TechInfoFragment extends FragmentDetailActivity {
 		currentReport.setSugar(esFieldSugar.getValue());
 		currentReport.setBreath(esFieldBreath.getValue());
 
-		getCurrentReport().setMinBloodPressure(
-				bloodPressureView.getSelectedMinValue());
-		getCurrentReport().setMaxBloodPressure(
-				bloodPressureView.getSelectedMaxValue());
+		currentReport.setMinBloodPressure(bloodPressureField.getLowValue());
+		currentReport.setMaxBloodPressure(bloodPressureField.getHighValue());
+
 	}
 
 	@Override
@@ -222,10 +70,9 @@ public class TechInfoFragment extends FragmentDetailActivity {
 		esFieldSugar.setValue(currentReport.getSugar());
 		esFieldBreath.setValue(currentReport.getBreath());
 
-		bloodPressureView.setSelectedMinValue(getCurrentReport()
-				.getMinBloodPressure());
-		bloodPressureView.setSelectedMaxValue(getCurrentReport()
-				.getMaxBloodPressure());
+		bloodPressureField.setValues(currentReport.getMaxBloodPressure(),
+				currentReport.getMinBloodPressure());
+
 	}
 
 }
---
+++ MadaReports/src/com/mdareports/ui/custom/bloodpressure/BloodPressureDialog.java
@@ -0,0 +1,118 @@
+package com.mdareports.ui.custom.bloodpressure;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import com.mdareports.R;
+import com.mdareports.utils.Logger;
+
+public class BloodPressureDialog extends RelativeLayout {
+	private String TAG = Logger.makeLogTag(getClass());
+
+	private EditText txtValueHigh;
+	private EditText txtValueLow;
+
+	public BloodPressureDialog(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		// inflate the layout
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.dialog_blood_pressure, this, true);
+
+		// set the views members
+		txtValueHigh = (EditText) findViewById(R.id.txtBloodPressureValueHigh);
+		txtValueLow = (EditText) findViewById(R.id.txtBloodPressureValueLow);
+
+		// change the seek-bar value each time the edit-text is changed
+		// txtValueHigh.addTextChangedListener(new TextWatcher() {
+		// @Override
+		// public void onTextChanged(CharSequence s, int start, int before,
+		// int count) {
+		// }
+		//
+		// @Override
+		// public void beforeTextChanged(CharSequence s, int start, int count,
+		// int after) {
+		// }
+		//
+		// @Override
+		// public void afterTextChanged(Editable s) {
+		// Logger.LOGE(TAG, "afterTextChanged: " + s.toString());
+		// /*
+		// * try { commonValue = Integer.parseInt(s.toString()); if
+		// * (commonValue > seekBar.getMax()) commonValue =
+		// * seekBar.getMax();
+		// *
+		// * seekBar.setProgress(commonValue); } catch (Exception e) {
+		// * Logger.LOGE(TAG, e.getMessage()); } s.toString();
+		// */
+		// }
+		// });
+
+	}
+
+	public void openDialog(Context context, String title,
+			DialogInterface.OnClickListener onPositiveClick) {
+		new AlertDialog.Builder(context)
+				.setView(this)
+				.setTitle(title)
+				.setPositiveButton(R.string.ok, onPositiveClick)
+				.setNegativeButton(R.string.cancel,
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								dialog.cancel();
+							}
+						}).show();
+	}
+
+	/**
+	 * Set the values of the field.
+	 * 
+	 * @param high
+	 *            - the high blood pressure value. the left value.
+	 * @param low
+	 *            - the low blood pressure value. the right value.
+	 */
+	public void setValues(int high, int low) {
+		txtValueHigh.setText(String.valueOf(high));
+		txtValueLow.setText(String.valueOf(low));
+	}
+
+	/**
+	 * Get the current values of the field
+	 */
+	public int getHighValue() {
+		ensureHighValueInRight();
+		return getValue(txtValueHigh);
+	}
+
+	public int getLowValue() {
+		ensureHighValueInRight();
+		return getValue(txtValueLow);
+	}
+
+	private int getValue(EditText txt) {
+		try {
+			return Integer.parseInt(txt.getEditableText().toString());
+		} catch (Exception e) {
+			return -1;
+		}
+	}
+
+	private void ensureHighValueInRight() {
+		int high = getValue(txtValueHigh);
+		int low = getValue(txtValueLow);
+		if (low > high) {
+			txtValueHigh.setText(String.valueOf(low));
+			txtValueLow.setText(String.valueOf(high));
+		}
+	}
+}
