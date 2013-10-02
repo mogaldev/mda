94bf7d0697b85e2da624eddf1e2decf62014a488
madargal@gmail.com <madargal@gmail.com@86056eeb-25c0-f91b-d89b-f80b2ee9bc02>
2013-10-02 21:29:32 +0000

    Added the HomeButton as enabled in the SettingsActivity
    git-svn-id: https://mada-reports.googlecode.com/svn/trunk@149 86056eeb-25c0-f91b-d89b-f80b2ee9bc02

--- MadaReports/src/com/mdareports/ui/activities/SettingsActivity.java
+++ MadaReports/src/com/mdareports/ui/activities/SettingsActivity.java
@@ -3,6 +3,9 @@ package com.mdareports.ui.activities;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+
+import com.actionbarsherlock.view.MenuItem;
 import com.mdareports.R;
 import com.mdareports.ui.fragments.SettingsFragment;
 
@@ -12,13 +15,26 @@ public class SettingsActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		
+		// Set the home button in the action bar as enabled
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
 
 		// Display the fragment instead of the FrameLayout in the
 		// activity_settings layout
-		getFragmentManager().beginTransaction()
-				.replace(R.id.settings_frame_layout, new SettingsFragment())
-				.commit();
+		getFragmentManager().beginTransaction().replace(R.id.settings_frame_layout,
+		                                                new SettingsFragment()).commit();
 
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 }
