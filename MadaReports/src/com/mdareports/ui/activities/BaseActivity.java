c0fc065073d777ace340b03b9e5155292fe8065d
Moshe Beladev <moshebe123@gmail.com>
2014-03-09 21:17:54 +0200

    Adding analytics tracking

--- MadaReports/src/com/mdareports/ui/activities/BaseActivity.java
+++ MadaReports/src/com/mdareports/ui/activities/BaseActivity.java
@@ -1,5 +1,7 @@
 package com.mdareports.ui.activities;
 
+import com.google.analytics.tracking.android.EasyTracker;
+
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -195,4 +197,24 @@ public class BaseActivity extends ActionBarActivity {
 		writeLongTimeMessage(getResources().getString(messageResId));
 	}
 
+	
+	// /////////////////////////////////////////
+	// --- Google Analytics Tracking ---
+	// /////////////////////////////////////////
+	
+	@Override
+	protected void onStart() {	
+		super.onStart();
+		EasyTracker.getInstance(this).activityStart(this);
+	}
+	
+	@Override
+	protected void onStop() {	
+		super.onStop();
+		EasyTracker.getInstance(this).activityStop(this);
+	}
+	
+	// /////////////////////////////////////////
+	// --- End of Google Analytics Tracking ---
+	// /////////////////////////////////////////
 }
--- MadaReports/src/com/mdareports/ui/activities/OldSettingsActivity.java
+++ MadaReports/src/com/mdareports/ui/activities/OldSettingsActivity.java
@@ -2,6 +2,8 @@ package com.mdareports.ui.activities;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+
+import com.google.analytics.tracking.android.EasyTracker;
 import com.mdareports.R;
 
 public class OldSettingsActivity extends PreferenceActivity {
@@ -11,9 +13,21 @@ public class OldSettingsActivity extends PreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences_settings);
-
-
 	}
+	
+	@Override
+	protected void onStart() {		
+		super.onStart();
+		EasyTracker.getInstance(this).activityStart(this);
+	}
+	
+	@Override
+	protected void onStop() {	
+		super.onStop();
+		EasyTracker.getInstance(this).activityStop(this);
+	}
+	
+	
 		
 
 
