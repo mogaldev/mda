e529bb0cb0a030d87a42b00d3fbcce45b0c3b419
Gal Madar <madargal@gmail.com>
2014-03-15 15:13:02 +0200

    update of MdaAnalytics class

--- MadaReports/src/com/mdareports/utils/MdaAnalytics.java
+++ MadaReports/src/com/mdareports/utils/MdaAnalytics.java
@@ -1,16 +1,21 @@
 package com.mdareports.utils;
 
 import android.app.Activity;
+import android.content.Context;
 
 import com.google.analytics.tracking.android.EasyTracker;
 
 public class MdaAnalytics {
 	
+	private static EasyTracker getEasyTrackerInstance(Context context) {
+		return EasyTracker.getInstance(context);
+	}
+	
 	public static void activityStart(Activity activity) {
-		EasyTracker.getInstance(activity).activityStart(activity);
+		getEasyTrackerInstance(activity).activityStart(activity);
 	}
 	
 	public static void activityStop(Activity activity) {
-		EasyTracker.getInstance(activity).activityStop(activity);
+		getEasyTrackerInstance(activity).activityStop(activity);
 	}
 }
