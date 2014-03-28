7497b1191fdb2a095d2a494a0e1be491e4fe8c22
Moshe Beladev <moshebe123@gmail.com>
2014-03-28 19:32:04 +0300

    Adding in-app purchase. Still not yet been tested. For testing we need to re-upload the application with the current permissions, add the "purchased" items and create test account to test buying.

--- MadaReports/src/com/mdareports/ui/fragments/HomeFragment.java
+++ MadaReports/src/com/mdareports/ui/fragments/HomeFragment.java
@@ -1,20 +1,41 @@
 package com.mdareports.ui.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.mdareports.R;
+import com.mdareports.utils.billing.BillingUtils;
 
 public class HomeFragment extends BaseFragment {
-	GoogleMap map;
+	BillingUtils billingUtils;
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		Activity activity = getActivity();
+		if (resultCode == activity.RESULT_OK) {
+			Toast.makeText(activity, "Thank you!", Toast.LENGTH_LONG).show();
+
+			if (billingUtils != null)
+				billingUtils.consumePurchasedItems(data);
+		}
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		billingUtils = new BillingUtils(getActivity());
+		billingUtils.bind();
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,72 +44,14 @@ public class HomeFragment extends BaseFragment {
 		final View rootView = inflater.inflate(R.layout.fragment_home,
 				container, false);
 
-		// Acquire a reference to the system Location Manager
-//		LocationManager locationManager = (LocationManager) getActivity()
-//				.getSystemService(Context.LOCATION_SERVICE);
-
-		// Define a listener that responds to location updates
-		// LocationListener locationListener = new LocationListener() {
-		// public void onLocationChanged(Location location) {
-		// // Called when a new location is found by the network location
-		// provider.
-		// //makeUseOfNewLocation(location);
-		// //map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(0, 0)));
-		// }
-		//
-		// public void onStatusChanged(String provider, int status, Bundle
-		// extras) {}
-		//
-		// public void onProviderEnabled(String provider) {}
-		//
-		// public void onProviderDisabled(String provider) {}
-		// };
-
-		// Register the listener with the Location Manager to receive location
-		// updates
-		// locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-		// 0, 0, locationListener);
-
-//		map = ((SupportMapFragment) getFragmentManager().findFragmentById(
-//				R.id.map)).getMap();
-
-		if (map != null) {
-			
-//			Criteria criteria = new Criteria();
-//			String bestProvider = locationManager.getBestProvider(criteria, false);
-//			Location loc = locationManager.getLastKnownLocation(bestProvider);
-//
-//			if (loc != null) {
-//				Log.e("getLastKnownLocation", "Location: " + loc.toString());
-//
-//				// map.setMyLocationEnabled(true);
-//
-//				map.moveCamera(CameraUpdateFactory.newLatLngZoom(
-//						new LatLng(loc.getLatitude(), loc.getLongitude()), 2));
-//			}
-//			
-
-			map.setMyLocationEnabled(true);
-			Marker hamburg = map.addMarker(new MarkerOptions().position(
-					new LatLng(-100, 50)).title("Hamburg"));
-			Marker kiel = map.addMarker(new MarkerOptions()
-					.position(new LatLng(-100, 120))
-					.title("Kiel")
-					.snippet("Kiel is cool")
-					.icon(BitmapDescriptorFactory
-							.fromResource(R.drawable.ic_launcher)));
-
-			map.setOnMapLongClickListener(new OnMapLongClickListener() {
-				@Override
-				public void onMapLongClick(LatLng point) {
-					map.addMarker(new MarkerOptions()
-							.position(point)
-							.title("You are here")
-							.icon(BitmapDescriptorFactory
-									.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
-				}
-			});
-		}
+		Button btnBuy = (Button) rootView.findViewById(R.id.btnBuy);
+		btnBuy.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (billingUtils != null)
+					billingUtils.buy("abc");
+			}
+		});
 
 		return rootView;
 	}
---
+++ MadaReports/src/com/mdareports/utils/billing/BillingUtils.java
@@ -0,0 +1,149 @@
+package com.mdareports.utils.billing;
+
+import java.util.ArrayList;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+
+import com.android.vending.billing.IInAppBillingService;
+
+public class BillingUtils {
+
+	private Activity activity;
+	private IInAppBillingService mService;
+	private final int API_VERSION = 3;
+	private String packageName;
+
+	public BillingUtils(Activity activity) {
+		this.activity = activity;
+		this.packageName = activity.getPackageName();
+	}
+
+	/**
+	 * Should be called onCreate
+	 */
+	public void bind() {
+		activity.bindService(new Intent(
+				"com.android.vending.billing.InAppBillingService.BIND"),
+				mServiceConn, Context.BIND_AUTO_CREATE);
+	}
+
+	/**
+	 * Should be called onDestroy
+	 */
+	public void unbind() {
+		if (mService != null) {
+			activity.unbindService(mServiceConn);
+		}
+	}
+
+	/**
+	 * Should be called onActivityResult
+	 * 
+	 * @param data
+	 *            - the returned intent to the activity.
+	 */
+	public void consumePurchasedItems(final Intent data) {
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					JSONObject json = new JSONObject(
+							data.getStringExtra("INAPP_PURCHASE_DATA"));
+					mService.consumePurchase(API_VERSION, packageName,
+							json.getString("purchaseToken"));
+				} catch (JSONException e) {
+					e.printStackTrace();
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}.start();
+	}
+
+	/**
+	 * Perform the actual buying (moving to the Google-Play purchase screen)
+	 * TODO: consider change to "subs" instead of "inapp". check if more changes
+	 * should be done because of this change.
+	 * 
+	 * @param item
+	 *            - the item to be bought, corresponding to the name in the
+	 *            developer console.
+	 */
+	public void buy(String item) {
+		try {
+			Bundle buyIntentBundle = mService.getBuyIntent(API_VERSION,
+					packageName, item, "inapp",
+					// TODO: replace with our key. maybe save encrypted or
+					// calculate it at runtime (google security recommendation)
+					"bGoa+V7g/ysDXvKwqq+JTFn4uQZbPiQJo4pf9RzJ");
+			PendingIntent pendingIntent = buyIntentBundle
+					.getParcelable("BUY_INTENT");
+			if (pendingIntent != null) {
+				activity.startIntentSenderForResult(
+						pendingIntent.getIntentSender(), 1001, new Intent(), 0,
+						0, 0);
+			}
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (SendIntentException e) {
+			e.printStackTrace();
+		}
+	}
+
+	ServiceConnection mServiceConn = new ServiceConnection() {
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			mService = null;
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			mService = IInAppBillingService.Stub.asInterface(service);
+
+			new Thread() {
+
+				@Override
+				public void run() {
+					try {
+						Bundle ownedItems = mService.getPurchases(API_VERSION,
+								packageName, "inapp", null);
+
+						int response = ownedItems.getInt("RESPONSE_CODE");
+						if (response == 0) {
+							ArrayList<String> purchaseDataList = ownedItems
+									.getStringArrayList("INAPP_PURCHASE_DATA_LIST");
+
+							if (purchaseDataList != null) {
+								for (String purchaseData : purchaseDataList) {
+									JSONObject json = new JSONObject(
+											purchaseData);
+									mService.consumePurchase(API_VERSION,
+											packageName,
+											json.getString("purchaseToken"));
+								}
+							}
+						}
+					} catch (RemoteException e) {
+						e.printStackTrace();
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+			}.start();
+		}
+	};
+
+}
