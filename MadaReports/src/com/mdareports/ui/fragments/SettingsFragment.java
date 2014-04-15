0e778782ff8d0074d6aa0cb34586c9e47700fec1
Gal Madar <madargal@gmail.com>
2014-04-15 22:52:32 +0300

    format

--- MadaReports/src/com/mdareports/ui/fragments/SettingsFragment.java
+++ MadaReports/src/com/mdareports/ui/fragments/SettingsFragment.java
@@ -9,14 +9,14 @@ import com.mdareports.R;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class SettingsFragment extends PreferenceFragment {
-	
+
 	@Override
-	public void onCreate(Bundle savedInstanceState) {		
-		super.onCreate(savedInstanceState);	
-		
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
 		getActivity().setTitle(R.string.title_settings);
-		
-		 addPreferencesFromResource(R.xml.preferences_settings);
+
+		addPreferencesFromResource(R.xml.preferences_settings);
 	}
-	
+
 }
